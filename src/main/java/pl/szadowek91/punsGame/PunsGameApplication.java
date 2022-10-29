351b782b2a6992162031203c20d46b6989fa6b8f
Partyk Granda <szadowek91@gmail.com>
2022-10-29 10:33:35 +0200

    Initial commit

---
+++ src/main/java/pl/szadowek91/punsGame/PunsGameApplication.java
@@ -0,0 +1,13 @@
+package pl.szadowek91.punsGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunsGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunsGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/szadowek91/punsGame/PunsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szadowek91.punsGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunsGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
