f59b499e10c4c05bc6c0b45ca85d01ed672ab069
PG <szadowek91@gmail.com>
2022-11-04 15:00:09 +0100

    chatMssgctrl cleaned up

--- src/main/java/pl/szadowek91/punsGame/controller/ChatMessageCtrl.java
+++ src/main/java/pl/szadowek91/punsGame/controller/ChatMessageCtrl.java
@@ -4,18 +4,9 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import pl.szadowek91.punsGame.model.ChatMessage;
-import pl.szadowek91.punsGame.service.WordService;
-
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class ChatMessageCtrl {
-
-    private WordService wordService;
-
-    public ChatMessageCtrl(WordService wordService) {
-        this.wordService = wordService;
-    }
 
     @MessageMapping("/chat")
     @SendTo("/topic/messages") // broker name/queue name
@@ -24,14 +15,4 @@ public class ChatMessageCtrl {
         return message;
     }
 
-    private String getActualWord(HttpSession session) {
-        try {
-            if (session.getAttribute("word") == null || session.getAttribute("word").equals("")) {
-                return "";
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return (String) session.getAttribute("word");
-    }
 }
